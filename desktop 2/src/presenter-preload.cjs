4f7fddb287cd0a03eb544eca699bf0b9ce52e536
dominionstarwfg <dominionstarwfg@gmail.com>
2026-08-24 18:06:01 -0500

    desktop: split presenter parity commands from core toolbar channel

--- desktop 2/src/presenter-preload.cjs
+++ desktop 2/src/presenter-preload.cjs
@@ -1,6 +1,11 @@
 const {contextBridge,ipcRenderer}=require('electron');
 
+const parityCommands=new Set(['layout','annotate','show-meeting','slide-control']);
+
 contextBridge.exposeInMainWorld('presenterBridge',Object.freeze({
-  command:value=>ipcRenderer.send('desktop:presenter-command',String(value||'')),
+  command:value=>{
+    const command=String(value||'');
+    ipcRenderer.send(parityCommands.has(command)?'desktop:presenter-parity-command':'desktop:presenter-command',command);
+  },
   resize:(width,height)=>ipcRenderer.send('desktop:presenter-resize',{width:Number(width),height:Number(height)})
 }));
