a22872dbae8d4b7ded26f6cd43e8c1865d5831b8
dominionstarwfg <dominionstarwfg@gmail.com>
2026-08-26 19:36:32 -0500

    chore(rebuild): remove legacy desktop trees and obsolete Meet QA artifacts

--- desktop 2/src/presenter-preload.cjs
+++
@@ -1,6 +0,0 @@
-const {contextBridge,ipcRenderer}=require('electron');
-
-contextBridge.exposeInMainWorld('presenterBridge',Object.freeze({
-  command:value=>ipcRenderer.send('desktop:presenter-command',String(value||'')),
-  resize:(width,height)=>ipcRenderer.send('desktop:presenter-resize',{width:Number(width),height:Number(height)})
-}));
